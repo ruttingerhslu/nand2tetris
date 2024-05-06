f240f068d48d6d8746c93fe903ce068cd902c167
Rafael Uttinger <tschese.smurf@gmail.com>
2024-05-06 15:25:35 +0200

    add correct indentation

--- compiler/compilation_engine.py
+++ compiler/compilation_engine.py
@@ -2,8 +2,6 @@ from jack_tokenizer import JackTokenizer
 
 class CompilationEngine:
     def __init__(self, read_file, write_file):
-        with open(write_file, 'r+') as file:
-            file.truncate(0)
         self._read_file = read_file
         self._write_file = open(write_file, 'w')
 
@@ -12,15 +10,20 @@ class CompilationEngine:
 
         self._last_token = ''
 
+        self._tab_count = 0
+
         self.compileClass()
 
     def printXMLToken(self, token):
-        self._write_file.write('<' + self.tokenizer.tokenType().lower() + '> ' +\
+        self._write_file.write(self.get_indent() + '<' + self.tokenizer.tokenType().lower() + '> ' +\
             token +\
             ' </' + self.tokenizer.tokenType().lower() + '>\n')
+    
+    def get_indent(self):
+        return "\t" * self._tab_count
 
     def printXMLTag(self, tag):
-        self._write_file.write(tag + '\n')
+        self._write_file.write(self.get_indent() + tag + '\n')
 
     def process(self, tokens):
         if self.tokenizer.hasMoreTokens():
@@ -48,6 +51,7 @@ class CompilationEngine:
     
     def compileClass(self):
         self.printXMLTag('<class>')
+        self._tab_count += 1
         self.process('class')
         self.process(self.tokenizer.identifier())
         self.process('{')
@@ -56,10 +60,12 @@ class CompilationEngine:
         while self.tokenizer.keyWord() in ['constructor', 'function', 'method']:
             self.compileSubroutineDec()
         self.process('}')
+        self._tab_count -= 1
         self.printXMLTag('</class>')
 
     def compileClassVarDec(self):
         self.printXMLTag('<classVarDec>')
+        self._tab_count += 1
         self.process(['static', 'field'])
         self.process(self.get_types())
         self.process(self.tokenizer.identifier())
@@ -67,10 +73,12 @@ class CompilationEngine:
             self.process(',')
             self.process(self.tokenizer.identifier())
         self.process(';')
+        self._tab_count -= 1
         self.printXMLTag('</classVarDec>')
     
     def compileSubroutineDec(self):
         self.printXMLTag('<subroutineDec>')
+        self._tab_count += 1
         self.process(['constructor', 'function', 'method'])
         types = self.get_types()
         types.append('void')
@@ -80,30 +88,36 @@ class CompilationEngine:
         self.compileParameterList()
         self.process(')')
         self.compileSubroutineBody()
+        self._tab_count -= 1
         self.printXMLTag('</subroutineDec>')
     
     def compileParameterList(self):
         if self.tokenizer.tokenType() in ['IDENTIFIER', 'KEYWORD']:
             self.printXMLTag('<parameterList>')
+            self._tab_count += 1
             self.process(self.get_types())
             self.process(self.tokenizer.identifier())
             while self.tokenizer.symbol() == ',':
                 self.process(',')
                 self.process(self.get_types())
                 self.process(self.tokenizer.identifier())
+            self._tab_count -= 1
             self.printXMLTag('</parameterList>')
 
     def compileSubroutineBody(self):
         self.printXMLTag('<subroutineBody>')
+        self._tab_count += 1
         self.process('{')
         while self.tokenizer.keyWord() == 'var':
             self.compileVarDec()
         self.compileStatements()
         self.process('}')
+        self._tab_count -= 1
         self.printXMLTag('</subroutineBody>')
     
     def compileVarDec(self):
         self.printXMLTag('<varDec>')
+        self._tab_count += 1
         self.process('var')
         self.process(self.get_types())
         self.process(self.tokenizer.identifier())
@@ -111,10 +125,12 @@ class CompilationEngine:
             self.process(',')
             self.process(self.tokenizer.identifier())
         self.process(';')
+        self._tab_count -= 1
         self.printXMLTag('</varDec>')
 
     def compileStatements(self):
         self.printXMLTag('<statements>')
+        self._tab_count += 1
         while self.tokenizer.keyWord() in ['let', 'if', 'while', 'do', 'return']:
             match self.tokenizer.keyWord():
                 case 'let':
@@ -127,10 +143,12 @@ class CompilationEngine:
                     self.compileDo()
                 case 'return':
                     self.compileReturn()
+        self._tab_count -= 1
         self.printXMLTag('</statements>')
 
     def compileLet(self):
         self.printXMLTag('<letStatement>')
+        self._tab_count += 1
         self.process('let')
         self.process(self.tokenizer.identifier())
         if self.tokenizer.symbol() == '[':
@@ -140,11 +158,12 @@ class CompilationEngine:
         self.process('=')
         self.compileExpression()
         self.process(';')
-        # remove identifier
+        self._tab_count -= 1
         self.printXMLTag('</letStatement>')
     
     def compileIf(self):
         self.printXMLTag('<ifStatement>')
+        self._tab_count += 1
         self.process('if')
         self.process('(')
         self.compileExpression()
@@ -157,10 +176,12 @@ class CompilationEngine:
             self.process('{')
             self.compileStatements()
             self.process('}')
+        self._tab_count -= 1
         self.printXMLTag('</ifStatement>')
 
     def compileWhile(self):
         self.printXMLTag('<whileStatement>')
+        self._tab_count += 1
         self.process('while')
         self.process('(')
         self.compileExpression()
@@ -168,32 +189,40 @@ class CompilationEngine:
         self.process('{')
         self.compileStatements()
         self.process('}')
+        self._tab_count -= 1
         self.printXMLTag('</whileStatement>')
 
     def compileDo(self):
         self.printXMLTag('<doStatement>')
+        self._tab_count += 1
         self.process('do')
         self.compileExpression()
         self.process(';')
+        self._tab_count -= 1
         self.printXMLTag('</doStatement>')
 
     def compileReturn(self):
         self.printXMLTag('<returnStatement>')
+        self._tab_count += 1
         self.process('return')
         self.compileExpression()
         self.process(';')
+        self._tab_count -= 1
         self.printXMLTag('</returnStatement>')
 
     def compileExpression(self):
         self.printXMLTag('<expression>')
+        self._tab_count += 1
         self.compileTerm()
         while self.tokenizer.symbol() in ['+', '-', '*', '/', '&', '|', '<', '>', '=']:
             self.process(self.tokenizer.symbol())
             self.compileTerm()
+        self._tab_count -= 1
         self.printXMLTag('</expression>')
     
     def compileTerm(self):
         self.printXMLTag('<term>')
+        self._tab_count += 1
         match self.tokenizer.tokenType():
             case 'INT_CONST':
                 self.process(self.tokenizer.intVal())
@@ -232,12 +261,15 @@ class CompilationEngine:
                 elif self.tokenizer.symbol() in ['-', '~']:
                     self.process(self.tokenizer.symbol())
                     self.compileTerm()
+        self._tab_count -= 1
         self.printXMLTag('</term>')
 
     def compileExpressionList(self):
         self.printXMLTag('<expressionList>')
+        self._tab_count += 1
         self.compileExpression()
         while self.tokenizer.symbol() == ',':
             self.process(',')
             self.compileExpression()
+        self._tab_count -= 1
         self.printXMLTag('</expressionList>')
