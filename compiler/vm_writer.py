1b73720192f9e2530986089b835b50dc56edf4f3
Rafael Uttinger <tschese.smurf@gmail.com>
2024-05-06 17:40:55 +0200

    actual vm cmds

--- compiler/vm_writer.py
+++ compiler/vm_writer.py
@@ -5,33 +5,33 @@ type Command = tuple('ADD', 'SUB', 'NEG', 'EQ', 'GT', 'LT', 'AND', 'OR', 'NOT')
 class VMWriter:
     def __init__(self, vm_file):
         self._file = open(vm_file, 'w')
-
-    def writePush(self, segment: PushSegment, index: int):
-        self._file.write('PUSH COMMAND')
     
+    def writePush(self, segment: PushSegment, index: int):
+        self._file.write(f'push {segment.lower()} {index}\n')
+
     def writePop(self, segment: PopSegment, index: int):
-        self._file.write('POP COMMAND')
+        self._file.write(f'push {segment.lower()} {index}\n')
 
     def writeArithmetic(self, command: Command):
-        self._file.write('ARITHMETIC COMMAND')
-    
+        self._file.write(f'{command.lower()}\n')
+
     def writeLabel(self, label: str):
-        self._file.write('(' + label + ')')
+        self._file.write(f'label {label}\n')
 
     def writeGoto(self, label: str):
-        self._file.write('GOTO')
-    
+        self._file.write(f'goto {label}\n')
+
     def writeIf(self, label: str):
-        self._file.write('IF CMD')
-    
+        self._file.write(f'if-goto {label}\n')
+
     def writeCall(self, name: str, nArgs: int):
-        self._file.write('CALL COMMAND')
-    
+        self._file.write(f'call {name} {nArgs}\n')
+
     def writeFunction(self, name: str, nVars: int):
-        self._file.write('FUNCTION COMMAND')
+        self._file.write(f'function {name} {nVars}\n')
 
     def writeReturn(self):
-        self._file.write('RETURN COMMAND')
+        self._file.write('return\n')
     
     def close(self):
         self._file.close()
